49ee6bfe21ab3ab142b7f5afeae452fccc6338f6
12atanGame <12atangame@gmail.com>
2026-06-18 13:10:10 +0800

    go 合併XIN

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< Updated upstream
 import { MessageService } from 'primeng/api';
 import { Toast } from 'primeng/toast';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Toast],
   providers: [MessageService],
+=======
+import { Toast } from "primeng/toast";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Toast],
+>>>>>>> Stashed changes
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
