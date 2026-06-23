6e098513249a17b9707a2717f4920e14eab0d9f9
ChiSiou <abc86872200@gmail.com>
2026-06-23 14:13:19 +0800

    fix interface

--- src/app/component/member/login/login.ts
+++ src/app/component/member/login/login.ts
@@ -25,16 +25,16 @@ export class Login {
     private memberService: MemberService,
     private routes: Router,
     private messageService: MessageService,
-  ) {}
+  ) { }
 
   GetloginApi(data: loginData) {
     this.memberService.login(data).subscribe({
       next: (res) => {
-        localStorage.setItem('token', res.token);
-        localStorage.setItem('roles', JSON.stringify(res.roles));
-        localStorage.setItem('activeRole', res.activeRole);
+        localStorage.setItem('token', res.Token);
+        localStorage.setItem('roles', JSON.stringify(res.Roles));
+        localStorage.setItem('activeRole', res.ActiveRole);
 
-        const decoded: any = jwtDecode(res.token);
+        const decoded: any = jwtDecode(res.Token);
         console.log('decoded token:', decoded);
         ///把後端回傳的 JWT token 存到瀏覽器的 localStorage 裡面
 
