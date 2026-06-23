066012ee45bd53e4a66f8ec86d170f3424eea026
EILL278 <newso317@yahoo.com>
2026-06-23 13:52:59 +0800

    api完成

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,6 +43,10 @@ export const routes: Routes = [
         path: 'payment/result',
         loadComponent: () => import('./component/payment-result/payment-result').then(m => m.PaymentResult)
       },
+      {
+        path: 'camp/:id/rental',
+        loadComponent: () => import('./component/camping-rental/camping-rental').then(m => m.CampingRentalComponent)
+      }
     ]
   },
   {
